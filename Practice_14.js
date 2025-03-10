7f42907ef7ff283282b538b4547930dd2381b07f
Samiul Alim <samiul202018@gmail.com>
2025-03-10 23:00:20 +0600

    Add files via upload

---
+++ Practice_14.js
@@ -0,0 +1,49 @@
+//ইফ-এলস | page 77
+
+//Problem 01
+const returnHomeTime = 5;
+if(returnHomeTime < 6){
+    console.log("I prepared Nasta for you.");
+} else{
+    console.log("Jharur Bari");
+};
+
+//Problem 02
+const isLoggedIn = true;
+if( isLoggedIn == true) {
+    console.log("Welcome!");
+}else{
+    console.log("Get Lost!");
+};
+
+//Problem 03
+const runningDistance = 5;
+if(runningDistance == 5){
+    console.log("Eat Chocolate");
+}else{
+    console.log("You will be Fat");
+}
+
+//Problem 04
+const marks = 85;
+if(marks > 80){
+    console.log("You will get a Bike!");
+}else{
+    console.log("No Bike for you.");
+};
+
+//Problem 05
+const movieTime = 10;
+if(movieTime < 9){
+    console.log("I will watch the movie");
+}else{
+    console.log("Sleep is Better!");
+};
+
+//Problem 06
+const temperature = 25;
+if(temperature >= 30){
+    console.log("On AC");
+}else{
+    console.log("Sleep with Combol.")
+};
