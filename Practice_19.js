2e078cde445a6a76ca8833f9940f80e9b63a4a12
Samiul Alim <samiul202018@gmail.com>
2025-03-11 23:07:49 +0600

    Add files via upload

---
+++ Practice_19.js
@@ -0,0 +1,26 @@
+//অ্যারে | page 93
+
+//Problem 01
+const numbers = [71, 72, 73, 74, 75, 76, 77, 78, 79];
+
+//Problem 02
+const fruit = ["আপেল", "কলা", "কমলা", "আম", "পেয়ারা"];
+
+//Problem 03
+const vegetables = ["আলু", "পেঁয়াজ", "বেগুন", "মুলা", "গাজর", "ট্মেটো", "ঢ্যাড়স", "লাউ", "শসা", "পটল"];
+const totalVeges = vegetables.length;
+console.log(totalVeges);
+
+//Problem 04
+const myFavMonies = ["Kabir Sing", "Jawan", "Money Heiest", "Lucky Bashkar", "Intasteller"];
+
+//Problem 05
+const oddNumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29];
+
+//Problem 06
+const colors = ["white", "black", "red", "yellow", "blue"];
+console.log(colors.length);
+
+//Problem 07
+const capitals = ["dhaka", "dilli", "islamabad", "tokiyo", "kualalampur", "kabul"];
+console.log(capitals.length);
---
+++ Practice_20.js
@@ -0,0 +1,33 @@
+////অ্যারে | page 96
+
+//Problem 01
+const friends = ["sabid", "ohi", "ratul", "jubayer", "tanvir", "sabbir"];
+const inIndexThree = friends[3];
+console.log(inIndexThree);
+
+//Problem 02
+const books = ["The richest man in babylon", "Marhaba JavaScript e Maro Thaba", "Rich Dad Poor Dad", "Atomic Habits", "Cash Machine", "Paradoxical Sajid", "Bangla 1st Paper"];
+const inIndexFive = books[5];
+console.log(inIndexFive);
+
+//Problem 03
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+numbers[7] = 30;
+console.log(numbers);
+
+//Problem 04
+const games = ["Free fire", "Pabge", "Candi Crash", "Temple run"];
+games[2] = "subway surfer";
+console.log(games);
+
+//Problem 05
+const familyMembers = ["Samiul", "Sabid", "Salma", "Rayhan"];
+console.log(familyMembers[3]);
+
+//Problem 06
+const contries = ["Saudi Arba", "India", "Afganistan", "Japan", "South Koria", "New Zeland", "Egept"];
+console.log(contries[4]);
+
+//Problem 07
+const metarials = ['Pen', "Book", "Computer", "Router"];
+console.log(metarials[7]);
