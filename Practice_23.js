f42324606b4955ef44859b22c9e67e06a93f1398
Samiul Alim <samiul202018@gmail.com>
2025-03-12 21:39:18 +0600

    Add files via upload

---
+++ Practice_23.js
@@ -0,0 +1,32 @@
+//লুপ | page 104
+
+
+//Problem 01
+const myFavSubjects = ["Bangla", "Math", "Physics", "Ict", "English"];
+for(const subject of myFavSubjects){
+    console.log(subject);
+};
+
+//Problem 02
+const myFavFoods = ["Biriyani", "Chicken", "Fish", "Vegetable"];
+for(const food of myFavFoods){
+    console.log(food);
+};
+
+//Problem 03
+const bornYears = [2005, 2016, 1989, 1980];
+for(const year of bornYears){
+    console.log(year);
+};
+
+//Problem 04
+const players = ["Tamim", "Emon", "Shanto", "Mushfiq", "Hrihdoy", "Mahamudullah", "Zaker", "Miraz", "Hasan", "Taskin", "Nahid"];
+for(const player of players){
+    console.log(player);
+};
+
+//Problem 05
+const examDates = ["5/12/2025", "8/12/2025", "9/12/2025", "12/12/2025"];
+for(const date of examDates){
+    console.log(date);
+};
