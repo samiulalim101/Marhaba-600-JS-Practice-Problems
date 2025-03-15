49ed5c376c28fb2baee3371aa9bfd236e5b00433
Samiul Alim <samiul202018@gmail.com>
2025-03-15 23:29:20 +0600

    Add files via upload

---
+++ Practice_33.js
@@ -0,0 +1,63 @@
+//ফাংশন | page 142
+
+//Problem 01
+function juceKhao(){
+    console.log("ai nao labur juce.");
+};
+juceKhao();
+
+//Problem 02
+function myRoutine(){
+    console.log("Wake up from bed");
+    console.log("Brush my teeths");
+    console.log("Ozu");
+    console.log("Namaz");
+    console.log("Breakfast");
+    console.log("college")
+};
+myRoutine();
+
+//Problem 03
+function familyMembers(){
+    console.log("Samiul");
+    console.log("Sabid");
+    console.log("Salma");
+    console.log("Rayhan");
+};
+
+//Problem 04
+function taskDone(){
+    console.log("Shoping");
+    console.log("Prayer");
+    console.log("Playing");
+    console.log("Reading");
+    console.log("Eating");
+};
+taskDone();
+
+//Problem 05
+function greetMe(){
+    console.log("Good Morning! Have a great day!");
+};
+greetMe();
+
+//Problem 06
+function getFavFriends(){
+    console.log("Ohi Tanvir Sabbir");
+};
+getFavFriends();
+
+//Problem 07
+function introduceMyself(){
+    const myself = {
+        name : "Samiul",
+        age : 18,
+        address : "Dhaka",
+        phone : 1784513655747,
+        height : 5.9,
+        favFood : "Biriyani"
+    }
+    console.log(myself);
+};
+introduceMyself();
+
---
+++ Practice_34.js
@@ -0,0 +1,57 @@
+//ফাংশন | page 149
+
+
+//Problem 01
+function totalAge(fatherAge, sonAge){
+    console.log(fatherAge + sonAge); 
+};
+totalAge(35, 18);
+
+//Problem 02
+function multiplication(num1 , num2){
+    console.log(num1 * num2);
+};
+multiplication(5, 5);
+
+//Problem 03
+function totalResult(sub1 , sub2 , sub3){
+    console.log(sub1 + sub2 + sub3);
+};
+totalResult(90, 70, 80);
+
+//Problem 04
+function calculateAge(dateOfBirth){
+    console.log(2025 - dateOfBirth);
+};
+calculateAge(2006);
+
+//Problem 05
+function numberOfLau(money){
+    console.log(money / 35);
+};
+numberOfLau(200);
+
+//Problem 06
+function average(num1, num2, num3, num4){
+    console.log((num1 + num2 + num3 + num4 ) / 4);
+};
+average(3, 66, 89, 44);
+
+//Problem 07
+function sellingPrice(cost){
+    console.log(cost + 250);
+};
+sellingPrice(500);
+
+//Problem 08
+function myFutureAge( dateOfBirth ){
+    console.log(dateOfBirth + 100);
+};
+myFutureAge(2005);
+
+//Problem 09
+function mobileUseTime(dailyUseTime){
+    console.log(dailyUseTime * 30);
+};
+mobileUseTime(3);
+
